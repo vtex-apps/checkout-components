8ac186bb73124165e046bc0058132e24313dcc20
Lucas Cordeiro <ecdb.lucas@gmail.com>
2020-04-22 12:10:48 -0300

    remove useless padding

--- react/ListGroup.tsx
+++ react/ListGroup.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 const ListGroup: React.FC = ({ children }) => {
   return (
     <div className="nr5 nr0-ns bt bb b--muted-4">
-      <div className="nl5 nl0-ns pa0" role="group">
+      <div className="nl5 nl0-ns" role="group">
         {children}
       </div>
     </div>
